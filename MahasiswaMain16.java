ccfb4a771ccf448eb879b064ba1cac63ad54b813
nadznyu <nadiaalya0107@gmail.com>
2026-02-26 00:21:03 +0700

    Percobaan 3 (konstruktor berparameter)

--- MahasiswaMain16.java
+++ MahasiswaMain16.java
@@ -12,6 +12,10 @@ public class MahasiswaMain16 {
         mhs1.updateIPK(3.60);
         mhs1.tampilkanInformasi();
 
+        Mahasiswa16 mhs2 = new Mahasiswa16("Annisa Nabila", "2141720160", 3.25, "TI 2L");
+        mhs2.updateIPK(3.30);
+        mhs2.tampilkanInformasi();
+
     }
 
     String nama;
