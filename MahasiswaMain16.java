fce6a6a52ebf2ca6d038e5be9ff9f8246b888b7e
nadznyu <nadiaalya0107@gmail.com>
2026-02-25 23:12:02 +0700

    Percobaan 2

---
+++ MahasiswaMain16.java
@@ -0,0 +1,49 @@
+public class MahasiswaMain16 {
+    public static void main(String[] args) {
+
+        Mahasiswa16 mhs1 = new Mahasiswa16();
+        mhs1.nama = "Muhammad Ali Farhan";
+        mhs1.nim = "2241720171";
+        mhs1.kelas = "SI 2J";
+        mhs1.ipk = 3.55;
+
+        mhs1.tampilkanInformasi();
+        mhs1.ubahKelas("SI 2K");
+        mhs1.updateIPK(3.60);
+        mhs1.tampilkanInformasi();
+
+    }
+
+    String nama;
+    String nim;
+    String kelas;
+    double ipk;
+
+    void tampilkanInformasi() {
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("IPK: " + ipk);
+        System.out.println("Kelas: " + kelas);
+    }
+
+    void ubahKelas(String kelasBaru) {
+        kelas = kelasBaru;
+    }
+
+    void updateIPK(double ipkBaru) {
+        ipk = ipkBaru;
+    }
+
+    String nilaiKinerja() {
+        if (ipk >= 3.5) {
+            return "Kinerja sangat baik";
+        } else if (ipk >= 3.0) {
+            return "Kinerja baik";
+        } else if (ipk >= 2.0) {
+            return "Kinerja cukup";
+        } else {
+            return "Kinerja kurang";
+        }
+    }
+
+}
